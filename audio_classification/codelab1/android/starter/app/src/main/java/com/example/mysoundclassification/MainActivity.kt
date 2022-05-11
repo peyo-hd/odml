08b2328e2344a8a774727119ec959bd6ce48dacc
Peter Yoon <peyo.hd@gmail.com>
2022-05-11 11:28:42 +0900

    complete codelab1
      https://codelabs.developers.google.com/codelabs/tflite-audio-classification-basic-android
    
      copy lite-model_yamnet_classification_tflite_1.tflite

--- audio_classification/codelab1/android/starter/app/src/main/java/com/example/mysoundclassification/MainActivity.kt
+++ audio_classification/codelab1/android/starter/app/src/main/java/com/example/mysoundclassification/MainActivity.kt
@@ -27,10 +27,10 @@ class MainActivity : AppCompatActivity() {
     var TAG = "MainActivity"
 
     // TODO 2.1: defines the model to be used
-    // var modelPath = "lite-model_yamnet_classification_tflite_1.tflite"
+    var modelPath = "lite-model_yamnet_classification_tflite_1.tflite"
 
     // TODO 2.2: defining the minimum threshold
-    // var probabilityThreshold: Float = 0.3f
+    var probabilityThreshold: Float = 0.3f
 
     lateinit var textView: TextView
 
@@ -45,42 +45,42 @@ class MainActivity : AppCompatActivity() {
         val recorderSpecsTextView = findViewById<TextView>(R.id.textViewAudioRecorderSpecs)
 
         // TODO 2.3: Loading the model from the assets folder
-        // val classifier = AudioClassifier.createFromFile(this, modelPath)
+        val classifier = AudioClassifier.createFromFile(this, modelPath)
 
         // TODO 3.1: Creating an audio recorder
-        // val tensor = classifier.createInputTensorAudio()
+        val tensor = classifier.createInputTensorAudio()
 
         // TODO 3.2: showing the audio recorder specification
-        // val format = classifier.requiredTensorAudioFormat
-        // val recorderSpecs = "Number Of Channels: ${format.channels}\n" +
-        //        "Sample Rate: ${format.sampleRate}"
-        // recorderSpecsTextView.text = recorderSpecs
+        val format = classifier.requiredTensorAudioFormat
+        val recorderSpecs = "Number Of Channels: ${format.channels}\n" +
+                "Sample Rate: ${format.sampleRate}"
+        recorderSpecsTextView.text = recorderSpecs
 
         // TODO 3.3: Creating
-        // val record = classifier.createAudioRecord()
-        // record.startRecording()
+        val record = classifier.createAudioRecord()
+        record.startRecording()
 
         Timer().scheduleAtFixedRate(1, 500) {
 
             // TODO 4.1: Classifing audio data
-            // val numberOfSamples = tensor.load(record)
-            // val output = classifier.classify(tensor)
+            tensor.load(record)
+            val output = classifier.classify(tensor)
 
             // TODO 4.2: Filtering out classifications with low probability
-            // val filteredModelOutput = output[0].categories.filter {
-            //     it.score > probabilityThreshold
-            // }
+            val filteredModelOutput = output[0].categories.filter {
+                 it.score > probabilityThreshold
+            }
 
             // TODO 4.3: Creating a multiline string with the filtered results
-            //val outputStr =
-            //    filteredModelOutput.sortedBy { -it.score }
-            //        .joinToString(separator = "\n") { "${it.label} -> ${it.score} " }
+            val outputStr =
+                filteredModelOutput.sortedBy { -it.score }
+                    .joinToString(separator = "\n") { "${it.label} -> ${it.score} " }
 
             // TODO 4.4: Updating the UI
-            //if (outputStr.isNotEmpty())
-                //runOnUiThread {
-                //    textView.text = outputStr
-                //}
+            if (outputStr.isNotEmpty())
+                runOnUiThread {
+                    textView.text = outputStr
+                }
         }
     }
 }
